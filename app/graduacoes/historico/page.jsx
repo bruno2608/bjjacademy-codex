b373e8c621bc913d38d1d0fd59d9a04c295a4b28
Bruno Alves França <brunoafranca97@gmail.com>
2025-12-02 17:26:48 -0300

    Split graduacoes views into dedicated routes

---
+++ app/graduacoes/historico/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Suspense } from 'react'
+
+import { GraduacoesStaffPageContent } from '../GraduacoesStaffPageContent'
+
+export default function GraduacoesHistoricoPage() {
+  return (
+    <Suspense fallback={<div className="p-6 text-bjj-gray-200">Carregando graduações...</div>}>
+      <GraduacoesStaffPageContent forcedView="historico" />
+    </Suspense>
+  )
+}
